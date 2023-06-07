69488a6fcea5386eac3f7fd1f875a5755b8665d0
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-06-07 17:12:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ function volume_sphere(event) {
   let radius = document.getElementById("radius").value;
   let volume = (4 / 3) * 3.14 * Math.pow(radius, 3);
   
-  document.getElementById("volume").value = volume.toFixed(2);
+  document.getElementById("volume").value = volume.toFixed(4); // Adjust precision here
 }
 
 window.onload = function() {
   document.getElementById('MyForm').addEventListener('submit', volume_sphere);
 };
+
